70698228c0489820c932529a37d757944cbdc590
jobmania <xzxz7003@naver.com>
2025-01-30 00:29:58 +0900

    first commit

---
+++ src/main/java/com/stock_concurrency/StockConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.stock_concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockConcurrencyApplication.class, args);
+    }
+
+}
