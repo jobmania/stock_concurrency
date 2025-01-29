2f3cca2ddcab041cf6155739fb5b29d80aadce82
jobmania <xzxz7003@naver.com>
2025-01-30 01:45:36 +0900

    비관적락(pesssimistic Lock) 을 통한 동시성 제어 : 락을 통한 제어

--- src/main/java/com/stock_concurrency/repository/StockRepository.java
+++ src/main/java/com/stock_concurrency/repository/StockRepository.java
@@ -1,7 +1,14 @@
 package com.stock_concurrency.repository;
 
 import com.stock_concurrency.domain.Stock;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT S FROM Stock S WHERE S.id = :id")
+    Stock findByIdWithPessimisticLock(Long id);
 }
---
+++ src/main/java/com/stock_concurrency/service/PessimisticLockStockService.java
@@ -0,0 +1,23 @@
+package com.stock_concurrency.service;
+
+import com.stock_concurrency.domain.Stock;
+import com.stock_concurrency.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PessimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    public PessimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+        stock.decrease(quantity);
+        stockRepository.save(stock);
+    }
+}
